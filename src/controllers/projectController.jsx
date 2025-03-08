dec301213cd6b9be95be4a3c1d1081e72e4ed1f8
DIEGO <diegoalejandrogutierrez77@gmail.com>
2025-03-07 23:50:35 -0500

    models and controllers change the data

---
+++ src/controllers/projectController.jsx
@@ -0,0 +1,16 @@
+
+// src/controllers/ProjectController.js
+
+import ProjectModel from './models/ProjectModel';
+
+export const getProjects = () => {
+  const projects = [
+    new ProjectModel(1, 'Proyecto 1', 'Descripción del proyecto 1', '/path/to/image1.jpg', 'http://linkalproyecto1.com'),
+    new ProjectModel(2, 'Proyecto 2', 'Descripción del proyecto 2', '/path/to/image2.jpg', 'http://linkalproyecto2.com'),
+  ];
+
+  return projects;
+};
+
+//se exporta la función getProjects que retorna un array de objetos de la clase ProjectModel con los datos de los proyectos.
+//models/ProjectModel.js
